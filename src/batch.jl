56487334a1c35e3c0d1d8da8a6dd732c4e63e1aa
Yao Lu <luyaocns@gmail.com>
2018-07-10 19:16:00 +0800

    tupseqbatch make gpu optional

--- src/batch.jl
+++ src/batch.jl
@@ -4,7 +4,7 @@ minibatch(x, batchsize) = Any[cview(x, ind) for ind in indbatch(x, batchsize)]
 
 minibatch(x, y, batchsize) = Any[(cview(x, ind), cview(y, ind)) for ind in indbatch(x, batchsize)]
 
-function tupseqbatch(Xs, batchsize, by = identity)
+function tupseqbatch(Xs, batchsize, by = identity, gpu = Flux.gpu)
     Xs′ = (by.(xs) for xs in minibatch(Xs, batchsize))
     Xs′′ = ([gpu(cview(xs, i)) for i in 1:ccount(xs)] for xs in Xs′)
 end
