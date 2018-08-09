581ff71a963f80e10acb5bd305082edd766e1f92
Yao Lu <luyaocns@gmail.com>
2018-08-09 11:01:43 +0800

    reset

--- src/predict.jl
+++ src/predict.jl
@@ -1,16 +1,16 @@
-function predseq(m, xs, catseqdim)
+function predseq(m, xs, catseqdim; reset::Bool = true)
     ys = m.(xs) # for y in ys, y: 1xB
     zs = [veccat(catseqdim, [Flux.data(cpu(ys[t][n])) for t in 1:length(ys)]) for n in 1:length(first(ys))]
-    Flux.reset!(m)
+    reset && Flux.reset!(m)
     return zs
 end
 
-function predseqbatch(m, Xs, catseqdim = 1)
+function predseqbatch(m, Xs, catseqdim = 1; reset::Bool = true)
     m′ = forwardmode(m)
     Ys = [predseq(m′, xs, catseqdim) for xs in Xs]
     ys = first(Ys)
     catdims = ndims.(ys)
     Zs = [cat(catdims[n], [Ys[b][n] for b in 1:length(Ys)]...) for n in 1:length(ys)]
-    Flux.reset!(m)
+    reset && Flux.reset!(m)
     return Zs
 end
