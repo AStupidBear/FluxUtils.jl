1e23d3680e623fcd4ccf50ce826b8f8231d999ab
Yao Lu <luyaocns@gmail.com>
2018-09-05 13:33:59 +0800

    ADAMW

--- src/optimizer.jl
+++ src/optimizer.jl
@@ -1,20 +1,23 @@
-using Flux.Optimise: optimiser, invdecay, descent, momentum, rmsprop, adam, clip
+using Flux.Optimise: optimiser, invdecay, descent, descentweightdecay, momentum, rmsprop, adam, clip, Param
 using Flux.Optimise: back!, runall, @progress, @interrupts
 
 @init @suppress begin
 
-Flux.Optimise.SGD(ps, η = 0.1f0; decay = 0, thresh = 0.5f0) =
-  optimiser(ps, p -> clip(p, thresh), p -> invdecay(p, decay), p -> descent(p,η))
+Flux.Optimise.ADAMW(ps, η = 1f-3; β1 = 0.9f0, β2 = 0.999f0, ϵ = 1f-8, decay = 0f0) =
+  optimiser(ps, p -> adam(p; η = η, β1 = β1, β2 = β2, ϵ = ϵ), p -> descentweightdecay(p, 1, decay))
+       
+Flux.Optimise.SGD(ps, η = 1f-1; decay = 0f0, thresh = 0.5f0) =
+  optimiser(ps, p -> clip(p, thresh), p -> invdecay(p, decay), p -> descent(p, η))
 
-Flux.Optimise.Momentum(ps, η = 1f-2; ρ = 0.9f0, decay = 0, thresh = 0.5f0) =
+Flux.Optimise.Momentum(ps, η = 1f-2; ρ = 0.9f0, decay = 0f0, thresh = 0.5f0) =
   optimiser(ps, p -> clip(p, thresh), p -> invdecay(p,decay), 
-                p -> momentum(p, ρ, η), p -> descent(p,1))
+                p -> momentum(p, ρ, η), p -> descent(p, 1))
 
-Flux.Optimise.RMSProp(ps, η = 1f-3; ρ = 0.9f0, ϵ = 1f-8, decay = 0, thresh = 0.5f0) =
+Flux.Optimise.RMSProp(ps, η = 1f-3; ρ = 0.9f0, ϵ = 1f-8, decay = 0f0, thresh = 0.5f0) =
   optimiser(ps, p -> clip(p, thresh), p -> rmsprop(p; η = η, ρ = ρ, ϵ = ϵ), 
                 p -> invdecay(p, decay), p -> descent(p, 1))
 
-Flux.Optimise.ADAM(ps, η = 1f-3; β1 = 0.9f0, β2 = 0.999f0, ϵ = 1f-08, decay = 0, thresh = 0.5f0) =
+Flux.Optimise.ADAM(ps, η = 1f-3; β1 = 0.9f0, β2 = 0.999f0, ϵ = 1f-8, decay = 0f0, thresh = 0.5f0) =
   optimiser(ps, p -> clip(p, thresh), p -> adam(p; η = η, β1 = β1, β2 = β2, ϵ = ϵ), 
                 p -> invdecay(p, decay), p -> descent(p, 1))
 
