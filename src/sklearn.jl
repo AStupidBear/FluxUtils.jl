d16deb0e1f288eb8590ab0013e95f42f53e1fc04
Yao Lu <luyaocns@gmail.com>
2018-09-11 11:39:26 +0800

    xy2data for 2d xy

--- src/sklearn.jl
+++ src/sklearn.jl
@@ -1,17 +1,18 @@
 export FluxNet
 abstract type FluxNet end
 
-function xy2data(x, y, batchsize, seqsize)
-    data = ((gpu.(eachcol(x[:, ib, is])), gpu.(eachcol(y[:, ib, is])))
-            for is in indbatch(1:size(x, 3), seqsize)
-            for ib in indbatch(1:size(x, 2), batchsize))
+function xy2data(x, y, batchsize, seqsize = 1)
+    if ndims(x) == 3
+        data = ((gpu.(eachcol(x[:, ib, is])), gpu.(eachcol(y[:, ib, is])))
+                for is in indbatch(1:size(x, 3), seqsize)
+                for ib in indbatch(1:size(x, 2), batchsize))
+    else
+        data = ((gpu.(eachcol(x[:, ib])), gpu.(eachcol(y[:, ib])))
+                for ib in indbatch(1:size(x, 2), batchsize))
+    end
 end
 
-function fit!(net::FluxNet, x, y; epochs = 1, 
-              batchsize = 50, seqsize = 300, 
-              validation_data = nothing, validation_split = 0.0,  
-              sample_weight = nothing, class_weight = nothing, 
-              shuffle = false, cb = [])
+function fit!(net::FluxNet, x, y; epochs = 1, batchsize = 50, seqsize = 300, cb = [])
     data = xy2data(x, y, batchsize, seqsize)
     Flux.@epochs epochs Flux.train!(partial(net.loss, net), data, net.opt; cb = [cugc, cb...])
 end
