b561cb45f0cf6222e59385604ec967ae823d7ff9
Yao Lu <luyaocns@gmail.com>
2018-09-13 10:24:03 +0800

    gpu32

--- src/convert.jl
+++ src/convert.jl
@@ -3,4 +3,6 @@ Adapt.adapt_(::Type{<:Array{T}}, xs::Array) where T <: AbstractFloat =
 
 float32(m) = mapleaves(x -> Flux.adapt(Array{Float32}, x), m)
 
+gpu32(m) = gpu(float32(m))
+
 forwardmode(m) = mapleaves(Flux.data, m)
