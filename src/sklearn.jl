1ec468bf88b9774c09a488a08a14a8934d87f163
Yao Lu <luyaocns@gmail.com>
2018-09-13 18:17:06 +0800

    reuse gpu

--- src/sklearn.jl
+++ src/sklearn.jl
@@ -3,11 +3,11 @@ abstract type FluxNet end
 
 function xy2data(x, y, batchsize, seqsize = 1)
     if ndims(x) == 3
-        data = ((eachcol(x[:, ib, is]), eachcol(y[:, ib, is]))
+        data = ((gpu.(eachcol(x[:, ib, is])), gpu.(eachcol(y[:, ib, is])))
                 for is in indbatch(1:size(x, 3), seqsize)
                 for ib in indbatch(1:size(x, 2), batchsize))
     else
-        data = ((eachcol(x[:, ib]), eachcol(y[:, ib]))
+        data = ((gpu.(eachcol(x[:, ib])), gpu.(eachcol(y[:, ib])))
                 for ib in indbatch(1:size(x, 2), batchsize))
     end
 end
@@ -22,10 +22,10 @@ function predict!(ŷ, m::FluxNet, x)
     for ib in indbatch(1:size(x, 2), mf.batchsize)
         if ndims(x) == 3
             for t in 1:size(x, 3)
-                ŷ[:, ib, t] = mf(x[:, ib, t])
+                ŷ[:, ib, t] = cpu(mf(gpu(x[:, ib, t])))
             end
         else
-            ŷ[:, ib] = mf(x[:, ib])
+            ŷ[:, ib] = cpu(mf(gpu(x[:, ib])))
         end
     end
 end
