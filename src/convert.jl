8e8196f9bec51f030690af1574bf7a513f0f09ae
Yao Lu <luyaocns@gmail.com>
2018-09-21 23:44:11 +0800

    float64

--- src/convert.jl
+++ src/convert.jl
@@ -1,9 +1,11 @@
-export forwardmode, float32, gpu32
+export forwardmode, float32, float64, gpu32
 
 Adapt.adapt_(::Type{<:Array{T}}, xs::Array) where T <: AbstractFloat = 
     isbits(xs) ? xs : convert(Array{T}, xs)
 
 float32(m) = mapleaves(x -> Flux.adapt(Array{Float32}, x), m)
+
+float64(m) = mapleaves(x -> Flux.adapt(Array{Float64}, x), m)
 
 gpu32(m) = gpu(float32(m))
 
