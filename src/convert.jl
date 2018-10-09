a9cfaf8abccb2cb44e3852d26742c6ea6c9ae17e
Yao Lu <luyaocns@gmail.com>
2018-10-09 10:44:40 +0800

    Flux.mapleaves type instability for AbstractArray{<:Number}

--- src/convert.jl
+++ src/convert.jl
@@ -1,12 +1,11 @@
 export forwardmode, float32, float64, gpu32
 
-Adapt.adapt_(::Type{<:Array{T}}, xs::Array) where T <: AbstractFloat = 
-    isbits(xs) ? xs : convert(Array{T}, xs)
+using Flux, Adapt
+Adapt.adapt_(::Type{<:Array{T}}, xs::AbstractArray) where T <: AbstractFloat = convert(Array{T}, xs)
+Flux.mapleaves(f, x::AbstractArray{<:Number}) = f(x)
 
 float32(m) = mapleaves(x -> Flux.adapt(Array{Float32}, x), m)
-
 float64(m) = mapleaves(x -> Flux.adapt(Array{Float64}, x), m)
-
 gpu32(m) = gpu(float32(m))
 
 forwardmode(m) = mapleaves(Flux.data, m)
