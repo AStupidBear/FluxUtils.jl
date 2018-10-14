3dc1341494065db2a17ba4c0d3f6dac3d8aede00
Yao Lu <luyaocns@gmail.com>
2018-10-14 15:39:51 +0800

    invokelatest

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -6,9 +6,11 @@ export +ᵇ, -ᵇ, *ᵇ, /ᵇ, ^ᵇ
 /ᵇ(xs...) = x ./ y
 ^ᵇ(x, y) = x.^y
 
+
 using Base.Broadcast: Broadcasted, materialize, broadcasted
 using Flux.Tracker: data, tracker, unbroadcast, track, Call, TrackedStyle, broadcast_rebuild, istracked
 import Flux.Tracker: ∇broadcast
+using MacroTools: postwalk
 
 @inline function ∇broadcast(f::typeof(+), args::Vararg{Any, N}) where {N}
     y = broadcast(f, data.(args)...)
@@ -57,11 +59,18 @@ end
 bc2ex(arg, n) = (ex = Symbol("x$(n[])"); n[] += 1; ex)
 
 const ∇EX2∇f = Dict{Expr, Function}()
+const ∇EX2COUNT = Dict{Expr, Int}()
+
+function real2float(ex)
+    postwalk(ex) do x
+        isa(x, Real) ? Float32(x) : x
+    end
+end
 
 function ∇bc(bc, n = Ref(1))
     ex = bc2ex(bc, n)
     args = [Symbol("x$i") for i in 1:(n[] - 1)]
-    ∇exs = Calculus.differentiate(ex, args)
+    ∇exs = real2float.(Calculus.simplify.(Calculus.differentiate(ex, args)))
     @debug "using Calculus.jl to differentiate broadcasting" ∇exs
     ∇fs = Function[]
     for ∇ex in ∇exs
@@ -83,20 +92,26 @@ gradable(arg) = true
 function Base.Broadcast.materialize(bc::Broadcasted{TrackedStyle})
     bc1 = Broadcast.flatten(bc)
     bc2 = Broadcast.flatten(broadcast_rebuild(bc))
-    if gradable(bc)
+    if gradable(bc) && !in(bc2.f, [+, -, *])
         ∇broadcast((bc2.f, ∇bc(bc)), bc1.args...)
     else
         ∇broadcast(bc2.f, bc1.args...)
     end
 end
 
-@inline function ∇broadcast(f∇f::Tuple{F, G}, args::Vararg{Any, N}) where {F, G, N}
+@noinline function ∇broadcast(f∇f::Tuple{F, G}, args::Vararg{Any, N}) where {F, G, N}
     f, ∇fs = f∇f
     y = broadcast(f, data.(args)...)
     eltype(y) <: Real || return y
     eltype(y) == Bool && return y
     function back(Δ)
-        Δargs = ntuple(i -> istracked(args[i]) ? ∇fs[i].(args...) : zero(Δ), Val(N))
+        Δargs = try
+            Δargs = ntuple(i -> istracked(args[i]) ? Δ .* ∇fs[i].(args...) : zero(Δ), Val(N))
+        catch
+            @warn "invokelatest ∇fs[i]"
+            ∇fs′ = ntuple(i -> (x...) -> Base.invokelatest(∇fs[i], x...), Val(N))
+            Δargs = ntuple(i -> istracked(args[i]) ? Δ .* ∇fs′[i].(args...) : zero(Δ), Val(N))
+        end
         dxs = map(unbroadcast, args, Δargs)
         return dxs
     end
