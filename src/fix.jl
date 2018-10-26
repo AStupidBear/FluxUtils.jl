c1ef34c034fb137c05fd1b4ac17797cb266fb7a9
Yao Lu <luyaocns@gmail.com>
2018-10-26 14:56:21 +0800

    fix ngradient delta

--- src/fix.jl
+++ src/fix.jl
@@ -14,3 +14,17 @@ end
 vecnorm2(x::TrackedArray, p::Real = 2) = sum(abs2.(x))
 
 Flux.gpu(x) = Flux.mapleaves(identity, x)
+
+function Flux.Tracker.ngradient(f, xs::AbstractArray...; δ = sqrt(eps()))
+    grads = zero.(xs)
+    for (x, Δ) in zip(xs, grads), i in 1:length(x)
+        tmp = x[i]
+        x[i] = tmp - δ / 2
+        y1 = f(xs...)
+        x[i] = tmp + δ / 2
+        y2 = f(xs...)
+        x[i] = tmp
+        Δ[i] = (y2 - y1) / δ
+    end
+    return grads
+end
