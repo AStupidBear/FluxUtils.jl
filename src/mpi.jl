53967b0e5b6ff64e65937763b07f9d2b66863017
Yao Lu <luyaocns@gmail.com>
2019-03-30 11:33:31 +0800

    drop julia-0.7

--- src/mpi.jl
+++ src/mpi.jl
@@ -16,25 +16,3 @@ function syncparam!(m)
     MPI.Bcast!(v, 0, MPI.COMM_WORLD)
     vec2net!(m, v)
 end
-
-if VERSION < v"1.0"
-    using Flux.Optimise: Param, call
-
-    function syncgrad(p::Param)
-        function ()
-            recvbuf = zero(p.Δ)
-            MPI.Allreduce!(p.Δ, recvbuf, MPI.SUM, MPI.COMM_WORLD)
-            p.Δ .= recvbuf ./ MPI.Comm_size(MPI.COMM_WORLD)
-        end
-    end
-
-    function Flux.Optimise.optimiser(ps, fs...)
-        fs = (syncgrad, fs...)
-        ps = [Param(p) for p in ps]
-        fs = map(ps) do p
-            os = map(f -> f(p), fs)
-            () -> foreach(call, os)
-        end
-        () -> foreach(call, fs)
-    end
-end
