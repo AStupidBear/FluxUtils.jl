f53a086d1e68b0b702143e21f48541ca64b89d59
Yao Lu <luyaocns@gmail.com>
2019-09-13 22:50:40 +0800

    WinsorNorm

--- src/layer.jl
+++ src/layer.jl
@@ -1,4 +1,4 @@
-export DenseReLU, GaussianNoise
+export DenseReLU, GaussianNoise, WinsorNorm
 
 DenseReLU(in, out; ka...) = Dense(in, out, relu; ka...)
 
@@ -16,6 +16,7 @@ function Flux.RNN(in::Integer, out::Integer, nlayers::Integer,
     Chain(layers...)
 end
 
+# GaussianNoise
 mutable struct GaussianNoise{F}
   stddev::F
   active::Bool
@@ -31,3 +32,40 @@ function (a::GaussianNoise)(x)
 end
 
 Flux._testmode!(a::GaussianNoise, test) = (a.active = !test)
+
+# WinsorNorm
+mutable struct WinsorNorm
+    θd::Vector{Float32}
+    θu::Vector{Float32}
+    μ::Vector{Float32}
+    σ::Vector{Float32}
+end
+
+WinsorNorm() = WinsorNorm(zeros(Float32, 0), zeros(Float32, 0), zeros(Float32, 0), zeros(Float32, 0))
+
+function (m::WinsorNorm)(x)
+    @unpack θd, θu, μ, σ = m
+    @. ifelse(isnan(x), μ, (clamp(x, θd, θu) - μ) / σ)
+end
+
+function fit!(m::WinsorNorm, x)
+    x = reshape(x, size(x, 1), :)
+    F, N = size(x)
+    θd = zeros(Float32, F)
+    θu = zeros(Float32, F)
+    μ = zeros(Float32, F)
+    σ = zeros(Float32, F)
+    @showprogress "winsorize..." for f in 1:F
+        y = filter(!isnan, x[f, :])
+        if !isempty(y)
+            θd[f], θu[f] = quantile(y, [0.01f0, 0.99f0])
+            clamp!(y, θd[f], θu[f])
+            μ[f], σ[f] = mean(y), std(y)
+            σ[f] = ifelse(isnan(σ[f]), 1f0, σ[f])
+        else
+            θd[f] = θu[f] = μ[f] = 0f0
+            σ[f] = 1f0
+        end
+    end
+    @pack! m = θd, θu, μ, σ
+end
