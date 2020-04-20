9f161e6645e3a25fc167a901c87ca29e9b8c7465
Yao Lu <luyaocns@gmail.com>
2020-04-20 17:08:55 +0800

    remove all

--- src/FluxUtils.jl
+++ src/FluxUtils.jl
@@ -1,34 +1,3 @@
-__precompile__(true)
-
 module FluxUtils
-
-using Printf, LinearAlgebra, Statistics, Distributed, Random
-using Flux, BSON, ProgressMeter, Parameters, Requires
-
-using Flux: glorot_uniform, gate, zeros, ones, stack, unsqueeze, chunk
-using Flux: param, mapleaves, reset!, loadparams!
-using Flux: RNNCell, LSTMCell, GRUCell, Recur, OneHotMatrix, OneHotVector
-import Flux: hidden, @treelike, testmode!
-
-using Flux.Optimise: update!, apply!, train!
-using Flux.Optimise: Optimiser, Params, WeightDecay, ϵ, runall, IdDict
-
-include("math.jl")
-include("batch.jl")
-include("layer.jl")
-include("recurrent.jl")
-include("util.jl")
-include("params.jl")
-include("vector.jl")
-include("sklearn.jl")
-include("train.jl")
-include("optimizer.jl")
-if isdefined(Flux, :CuArrays)
-    include("cuda.jl")
-end
-
-function __init__()
-    @require MPI="da04e1cc-30fd-572f-bb4f-1f8673147195" include("mpi.jl")
-end
 
 end
--- src/cuda.jl
+++
@@ -1,9 +0,0 @@
-using Flux: CuArrays
-
-using Flux.CuArrays: CuArray, CUBLAS
-
-# TODO: move this to Flux.jl
-function Base.:(*)(A::CuArrays.CuMatrix, B::OneHotMatrix{CuArrays.CuArray{OneHotVector,1}})
-    I = CuArrays.CuArray{UInt32, 1}(B.data.buf, 2 .* B.data.dims, offset = B.data.offset)[1:2:end]
-    A[:, Array(I)]
-end
