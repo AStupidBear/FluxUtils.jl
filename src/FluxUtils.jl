1e895eb02181f91cbdab3d3ed0354851a5ff8430
Yao Lu <luyaocns@gmail.com>
2020-04-20 17:09:08 +0800

    WinsorNorm

--- src/FluxUtils.jl
+++ src/FluxUtils.jl
@@ -1,3 +1,32 @@
 module FluxUtils
 
+WinsorNorm() = WinsorNorm(zeros(Float32, 0), zeros(Float32, 0), zeros(Float32, 0), zeros(Float32, 0))
+
+function (m::WinsorNorm)(x)
+  @unpack θd, θu, μ, σ = m
+  @. ifelse(isnan(x), μ, (clamp(x, θd, θu) - μ) / σ)
+end
+
+function fit!(m::WinsorNorm, x)
+  x = reshape(x, size(x, 1), :)
+  F, N = size(x)
+  θd = zeros(Float32, F)
+  θu = zeros(Float32, F)
+  μ = zeros(Float32, F)
+  σ = zeros(Float32, F)
+  @showprogress "winsorize..." for f in 1:F
+      y = filter(!isnan, x[f, :])
+      if !isempty(y)
+          θd[f], θu[f] = quantile(y, [0.01f0, 0.99f0])
+          clamp!(y, θd[f], θu[f])
+          μ[f], σ[f] = mean(y), std(y)
+          σ[f] = ifelse(isnan(σ[f]), 1f0, σ[f])
+      else
+          θd[f] = θu[f] = μ[f] = 0f0
+          σ[f] = 1f0
+      end
+  end
+  @pack! m = θd, θu, μ, σ
+end
+
 end
